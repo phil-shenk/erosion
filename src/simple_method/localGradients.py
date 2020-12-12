ac36502669144687a68a115fa36f2673704aa02e
phil-shenk <shenk.philip@gmail.com>
2020-12-11 22:39:10 -0500

    decent results with numpy gradient and my local gradient, needs optimization though

--- src/simple_method/localGradients.py
+++ src/simple_method/localGradients.py
@@ -15,12 +15,17 @@ width  = 200
 length = 200
 gravity = 10.0
 
+use_numpy_gradients = False
+showdrops = False
+
 xs = np.linspace(-np.pi/2,np.pi/2,width)
 ys = np.linspace(-np.pi/2,np.pi/2,length)
 xx, yy = np.meshgrid(xs,ys)
 #grid = xx**2 + yy**2
-grid = v.calculateVoronoi(width,length,2,2)
+grid = -1.0*v.calculateVoronoi(width,length,2,2)
+prev_grid = grid.copy()
 grad = calculateGradient(grid)
+
 
 # just tuning these for now instead of more well-thought-out gravity things
 gradientMultiplier = 0.9
@@ -35,6 +40,7 @@ def changeGrid(x,y,dsediment):
 
     # if point is within grid
     if not (x < 0 or x >= width or y<0 or y>=length):
+            
         neighbors = np.empty(9)
         neighbors[:] = np.NaN
         validcount = 0
@@ -61,26 +67,87 @@ def changeGrid(x,y,dsediment):
                 for j in range(-1,2):
                     if j+y>=0 and j+y<length:
                         # (i+x, j+y) is a valid tile, set its value to the local avg + the appropriate chunk of sediment
-                        #grid[i+x][j+y] = avg + ds # spread evenly over neighbors #TODO: try other vals
-                        grid[i+x][j+y] = (grid[i+x][j+y] + avg)/2.0 + ds
+                        # set em all to the avg
+                        grid[i+x][j+y] = avg + ds
+                        #grid[i+x][j+y] += ds # spread evenly over neighbors #TODO: try other vals
+                        # average the current point with the average.. and add ds
+                        #grid[i+x][j+y] = (grid[i+x][j+y] + avg)/2.0 + ds
+                        #if(i==0 and j==0):
+                        #    grid[i+x][j+y] += 9*ds
+        """ update smoothly (works for numpy gradients.. but not my gradients because it makes the whole region flat..)
+        # now iterate over neighbors to update their values
+        for i in range(-1,2):
+            if i+x>=0 and i+x < width:
+                for j in range(-1,2):
+                    if j+y>=0 and j+y<length:
+                        # (i+x, j+y) is a valid tile, set its value to the local avg + the appropriate chunk of sediment
+                        grid[i+x][j+y] = avg + ds
+        """
 def getGrid(x,y):
     if (x < 0 or x >= width or y<0 or y>=length):
         return 0.0
     else:
-        return grid[x][y]
+        return prev_grid[x][y]
+
+def localNegativeGradient(x,y):
+    """
+    find gradient at given point using surrounding 8 adjacent cells \n
+    z1 | z2 | z3 \n
+    z4 | z5 | z6 \n
+    z7 | z8 | z9 \n
+    im gonna try something like this: \n
+    dz/dx = 2*(z6-z4) + z3 - z1 + z9 - z7 \n
+    dz/dy = 2*(z2-z8) + z3 - z9 + z1 - z7 \n
+    not using np.gradient because I think that does the whole grid.. could run it on just the 3x3 subgrid though
+    """
+    #TODO: handle edges better
+    #TODO: actually i think i should just kill drops when they're near the edge
+
+    # i'm sure there's a more elegant way to do this
+    ix = int(x)
+    iy = int(y)
+    z1 = getGrid(ix-1,iy-1)
+    z2 = getGrid(ix,iy-1)
+    z3 = getGrid(ix+1,iy-1)
+    z4 = getGrid(ix-1,iy)
+    #z5 = getGrid(ix,iy)
+    z6 = getGrid(ix+1,iy)
+    z7 = getGrid(ix-1,iy+1)
+    z8 = getGrid(ix,iy+1)
+    z9 = getGrid(ix+1,iy+1)
+    #z1,z2,z3,z4,z6,z7,z8,z9)
+    dzdx = -1.0*(2*(z6-z4) + z3 - z1 + z9 - z7) / 8.0
+    dzdy = -1.0*(2*(z2-z8) + z3 - z9 + z1 - z7) / 8.0
+
+    # clip em
+    clipval = 0.02
+    if np.abs(dzdx)>clipval:
+        dzdx = clipval*np.sign(dzdx)
+    if np.abs(dzdy)>clipval:
+        dzdy = clipval*np.sign(dzdy)
+
+    #print("loc",dzdx,dzdy)
+    return dzdx, -1.0*dzdy
+
+def localNumpyNegativeGradient(x,y):
+    if(int(x) <= 0 or int(x) >= width-1 or int(y)<=0 or int(y)>=length-1):
+        return 0.0,0.0
+    localgrid = np.array(prev_grid[int(x)-1:int(x)+2, int(y)-1:int(y)+2])
+    localgrad = np.gradient(localgrid)
+    dzdx = -1.0*localgrad[0][1][1]
+    dzdy = -1.0*localgrad[1][1][1]
+    return dzdx,dzdy
 
 def getNegativeGradient(x,y):
     global grad
     gx = -1*grad[0][int(x)][int(y)]
     gy = -1*grad[1][int(x)][int(y)]
-
-    #"""artificially clip gradient if it's too big
+    #"""artificially clip gradient
     clipval = 0.02
     if np.abs(gx)>clipval:
         gx = clipval*np.sign(gx)
     if np.abs(gy)>clipval:
         gy = clipval*np.sign(gy)
-
     return gx,gy
 
 def stepDrop(x,y,vx,vy,liquid,sediment):
@@ -93,13 +160,19 @@ def stepDrop(x,y,vx,vy,liquid,sediment):
     dzdx = 0
     dzdy = 0
     # get grid NEGATIVE gradient at drop position ** w.r.t. GRID ** (rise / 1 grid unit 'run')
-    dzdx, dzdy = getNegativeGradient(x,y)
+    if(use_numpy_gradients):
+        dzdx, dzdy = getNegativeGradient(x,y)
+    else:
+        dzdx, dzdy = localNegativeGradient(x,y)
+        #dzdx, dzdy = localNumpyNegativeGradient(x,y)
 
     # move drop according to gradient #TODO: incorporate momentum
     dx = dzdx*gravity + vx*momentumMultiplier
     dy = dzdy*gravity + vy*momentumMultiplier
 
     new_speed = np.sqrt(dx**2 + dy**2)
+
+    #print(prev_speed,new_speed)
 
     # max slope = magnitude of gradient
     slope = np.sqrt(dzdx**2 + dzdy**2)
@@ -132,6 +205,7 @@ def anim():
     for i in range(numdrops): 
         global grad # only for numpy gradients
         global grid
+        global prev_grid
         x = np.random.randint(width-1)
         y = np.random.randint(length-1)
         dx = (np.random.ranf()*2.0 - 1.0) / 10.0
@@ -139,7 +213,9 @@ def anim():
         liquid = 1.0
         sediment = 0.0
         # #TODO: definitely should just calculate gradient locally instead of using whole grid..
-        grad = calculateGradient(grid)
+        # or build up a list of modifications, and apply ONLy those after each drop
+        if(use_numpy_gradients):
+            grad = calculateGradient(grid)
         #for j in range(100):
         j=0
         meas = 100.0
@@ -158,9 +234,14 @@ def anim():
             x,y,dx,dy,liquid,sediment = stepDrop(x,y,dx,dy,liquid,sediment)
             buffer = 3
             
+            if(showdrops):
+                s.mlab_source.scalars = grid
+                yield
+
             if (int(x) < 0+buffer or int(x) >= width-buffer or int(y)<0+buffer or int(y)>=length-buffer):
                 break
-            
+        #hmm... maybe just save previous gradient locally too? #TODO
+        prev_grid = grid.copy()
         # put all sediment where the walker finishes
         depositSediment(x,y,sediment)
         
@@ -177,3 +258,5 @@ def anim():
 start = time.time()
 anim()
 mlab.show()
+
+print(getNegativeGradient(14,14))
